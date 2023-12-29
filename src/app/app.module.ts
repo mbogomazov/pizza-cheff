6b23f38f47737631bb59aa8692b791d4886abd1c
Software Engineer <software.engineer.student.acc@gmail.com>
2023-12-29 22:22:24 +0300

    remove initial component & add start version of header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [HeaderComponent],
 })
-export class AppModule {}
+export class AppModule { }
